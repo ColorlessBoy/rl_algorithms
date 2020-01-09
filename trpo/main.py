346def8cd687a2d3784fadcfc693e7f8ce8cc929
Peng Lingwei <penglingwei@zju.edu.cn>
2020-01-09 20:38:22 +0800

    construct utils.py in ppo and trpo

--- trpo/main.py
+++ trpo/main.py
@@ -1,73 +1,14 @@
 import gym
-import numpy as np
 import torch
 
+from utils import EnvSampler
 from models import PolicyNetwork, ValueNetwork
 from trpo import TRPO
-
-import random
-from collections import namedtuple
 
 import os
 import csv
 from time import time
 
-# Taken from
-# https://github.com/pytorch/tutorials/blob/master/Reinforcement%20(Q-)Learning%20with%20PyTorch.ipynb
-
-Transition = namedtuple('Transition', 
-            ('state', 'action', 'reward', 'next_state', 'mask'))
-
-class Memory(object):
-    def __init__(self):
-        self.memory = []
-
-    def push(self, *args):
-        self.memory.append(Transition(*args))
-
-    def sample(self):
-        return Transition(*zip(*self.memory))
-
-    def __len__(self):
-        return len(self.memory)
-
-class EnvSampler(object):
-    def __init__(self, env, max_episode_step=1000):
-        self.env = env
-        self.max_episode_step = max_episode_step
-        self.env_init()
-        self.action_scale = (env.action_space.high - env.action_space.low)/2
-        self.action_bias = (env.action_space.high + env.action_space.low)/2
-    
-    # action_encode and action_decode project action into [-1, 1]^n
-    def action_encode(self, action):
-        return (action - self.action_bias)/self.action_scale
-    
-    def action_decode(self, action_):
-        return action_ * self.action_scale + self.action_bias
-    
-    def env_init(self):
-        self.state = self.env.reset()
-        self.done = False
-        self.episode_step = 1
-    
-    def __call__(self, get_action, batch_size):
-        # I suggest batch_size to be the multiple of max_episode_step.
-        memory = Memory()
-        batch_reward = 0.0
-        for _ in range(batch_size):
-            action_ = get_action(self.state)
-            action =self.action_decode(action_)
-            next_state, reward, self.done, _ = self.env.step(action) 
-            # The env will automatically clamp action into [action_space.low, action_space.high]^n
-            batch_reward += reward
-            mask = 1.0 if not self.done else 0.0
-            memory.push(self.state, action_, reward, next_state, mask)
-            self.state = next_state
-            self.episode_step += 1
-            if self.done or self.episode_step > self.max_episode_step:
-                self.env_init()
-        return batch_reward, memory.sample()
 
 # The properties of args:
 # 1. env_name (default = 'HalfCheetah-v2')
@@ -94,7 +35,6 @@ def main(args):
     torch.manual_seed(args.seed)
     torch.cuda.manual_seed_all(args.seed)
     torch.manual_seed(args.seed)
-    np.random.seed(args.seed)
     env.seed(args.seed)
 
     # 2.Create actor, critic, EnvSampler() and TRPO.
