53a411480811c62c2069ea5133ca107c39a7a716
colorlessboy <501868024@qq.com>
2020-01-10 19:16:32 +0800

    Too slow SampleEnv().

--- ppo/exp-ppo.sh
+++ ppo/exp-ppo.sh
@@ -5,6 +5,7 @@ do
     for algo in local_ppo global_ppo ;
     do
         python parallel_main.py --env_name $env_name \
+                                --alg $algo \
                                 --agent 12 \
                                 --device cuda
     done
